a5319a7a6bcbadc27a2c2fbe83af4b89f65d6e17
Tiphaine BESNARD <tiphaine.besnard@etu.univ-nantes.fr>
2019-01-10 18:56:36 +0100

    Update Tri listeUtilitaire

--- Carte.cpp
+++ Carte.cpp
@@ -64,7 +64,7 @@ void Carte::ajouterBatimentPlace(BatimentPlace bp)
 	this->listeBatimentsPlaces.push_back(bp);
 }
 
-void Carte::placerBatiment(Batiment b, pair<int, int> c)
+bool Carte::placerBatiment(Batiment b, pair<int, int> c)
 {
 	bool condition = true;
 	if (c.first < 0 || c.second < 0) {
@@ -102,7 +102,7 @@ void Carte::placerBatiment(Batiment b, pair<int, int> c)
 			cout << "Place occupée" << endl;
 		}
 	}
-	
+	return condition;
 }
 
 //Tiphaine
@@ -116,43 +116,89 @@ void Carte::calculCoeff(vector<Batiment> listeBatiments) {
 		if (listeBatiments[i].getSpecificite() < 0) {
 			
 			float spe = -listeBatiments[i].getSpecificite();
-
 			// nombre de case prise/(largeur*hauteur) * nombre utilitaire 		
 			float coeff = sizeBatiment/(hauteur * largeur)*spe;
 			listeCoeffResidentiel.push_back(pair<Batiment, float>(listeBatiments[i], coeff));
 		}
 		else {
 			float coeff = sizeBatiment / (hauteur * largeur);
-			listeCoeffUtilitaire.push_back(pair<Batiment, float>(listeBatiments[i], coeff));
+			listeUtilitaire.push_back(listeBatiments[i]);
 		}
 	}
 }
 
-//Tiphaine
+void Carte::afficherBatCoeff() {
+	for (int i = 0; i < listeBatimentUtilitaireTriee.size(); i++) {
+		for (int j = 0; j < listeBatimentUtilitaireTriee[i].size(); j++) {
+			cout << "Batiment de groupe : " <<i << " - Spe : " << listeBatimentUtilitaireTriee[i][j].getSpecificite() << "Largeur : "<< listeBatimentUtilitaireTriee[i][j].getLargeur() << endl;
+		}
+	}
+	/*for (int i = 0; i < listeBatimentUtilitaireTriee.size(); i++) {
+		for (int j = 0; j < listeBatimentUtilitaireTriee[i].size(); j++) {
+			cout << "Batiment de groupe : " << i << " - Spe : " << listeBatimentUtilitaireTriee[i][j].getSpecificite()<< endl;
+		}
+	}*/
+}
+
 void Carte::triBatimentUti() {
-		for (int i = 0; i < listeCoeffUtilitaire.size(); i++)
+	for (int i = 0; i < listeUtilitaire.size(); i++)
+	{
+		for (int j = i; j < listeUtilitaire.size(); j++)
 		{
-			for (int j = i; j < listeCoeffUtilitaire.size(); j++) 
-			{
-				if (listeCoeffUtilitaire[j].second < listeCoeffUtilitaire[i].second)  /* si on inverse le signe d'inégalité
-											  on aura le trie décroissant */
-				{
-					auto valeurtemporaire = listeCoeffUtilitaire[i];
-					listeCoeffUtilitaire[i] = listeCoeffUtilitaire[j];
-					listeCoeffUtilitaire[j] = valeurtemporaire;
-				}
+			if (listeUtilitaire[j].getSpecificite() < listeUtilitaire[i].getSpecificite()) {
+				auto valeurtemporaire = listeUtilitaire[i];
+				listeUtilitaire[i] = listeUtilitaire[j];
+				listeUtilitaire[j] = valeurtemporaire;
+		
+			}	
+		}
+	}
+
+	int specificite = 0;
+	vector<Batiment> bat;
+	listeBatimentUtilitaireTriee.push_back(bat);
+
+	for (int i = 0; i < listeUtilitaire.size(); i++) {
+		if (listeUtilitaire[i].getSpecificite()-1 != specificite) {
+			vector<Batiment> bat;
+			listeBatimentUtilitaireTriee.push_back(bat);
+			specificite += 1;
+			bat.push_back(listeUtilitaire[i]);
+		}
+		else {
+			listeBatimentUtilitaireTriee[specificite].push_back(listeUtilitaire[i]);
+		}
+	}
+
+	for (int i = 0; i < listeBatimentUtilitaireTriee.size(); i++)
+	{
+		for (int j =0; j < listeBatimentUtilitaireTriee[i].size()-1; j++)
+		{
+			if (listeBatimentUtilitaireTriee[i][j].getLargeur() < listeBatimentUtilitaireTriee[i][j+1].getLargeur()) {
+				auto valeurtemporaire = listeBatimentUtilitaireTriee[i][j];
+				listeBatimentUtilitaireTriee[i][j+1] = listeBatimentUtilitaireTriee[i][j];
+				listeBatimentUtilitaireTriee[i][j] = valeurtemporaire;
 			}
 		}
+	}
 }
 
-//Tiphaine
-void Carte::afficherBatCoeff() {
 
-	for (int i = 0; i < listeCoeffResidentiel.size(); i++) {
-		cout << "Batiment : " << i << " - Spe : " << listeCoeffResidentiel[i].first.getSpecificite() << " - Coeff : " << listeCoeffResidentiel[i].second << endl;
+
+bool Carte::placerBatimentOpti(pair<int, int> pointA, pair<int, int> pointB)
+{
+	Batiment bestBat = listeCoeffResidentiel[0].first;
+
+	if (placerBatiment(bestBat, pair<int, int>(pointB.first / 2 - bestBat.getLargeur() / 2, pointB.second / 2 - bestBat.getHauteur() / 2)))
+	{
+		placerBatimentOpti(pair<int, int>(pointA.first, pointA.second), pair<int, int>(pointB.first / 2, pointB.second / 2));        //0,0
+		placerBatimentOpti(pair<int, int>(pointB.first / 2, pointA.second), pair<int, int>(pointB.first, pointB.second / 2));       //0,1
+		placerBatimentOpti(pair<int, int>(pointA.first, pointB.second / 2), pair<int, int>(pointB.first / 2, pointB.second));       //1,0
+		placerBatimentOpti(pair<int, int>(pointB.first / 2, pointB.second / 2), pair<int, int>(pointB.first, pointB.second));       //1,1
+		return true;
 	}
-
-	for (int i = 0; i < listeCoeffUtilitaire.size(); i++) {
-		cout << "Batiment : " << i << " - Spe : " << listeCoeffUtilitaire[i].first.getSpecificite() << " - Coeff : " << listeCoeffUtilitaire[i].second << endl;
+	else
+	{
+		return false;
 	}
 }
